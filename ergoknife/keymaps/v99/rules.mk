2d892540430e5ccf99c466f7fce44e316749ed45
ouser555 <95702400+ouser555@users.noreply.github.com>
2023-01-23 16:15:43 +0800

    Delete ergoknife directory

--- ergoknife/keymaps/v99/rules.mk
+++
@@ -1,6 +0,0 @@
-VIA_ENABLE          = yes
-POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = custom
-#SRC += v99slim.c
-SRC += keyboards/ergoknife/keymaps/v99/i2cc.c
-SRC += keyboards/ergoknife/keymaps/v99/v99i2chw.c
